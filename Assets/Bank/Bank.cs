461a8e243ee65611429374efa6ba77e23a471cba
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-12-11 17:14:15 +0200

    added basic ui

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,27 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
-    [SerializeField] int startingBalance = 150;
+    [SerializeField] int startingBalance = 1000;
+
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
     void Awake()
     {
-        currentBalance = startingBalance;    
+        currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (currentBalance < 0) 
         {
@@ -34,6 +40,11 @@ public class Bank : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = $"Gold: {currentBalance}";
     }
 
 }
